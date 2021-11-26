91df3bc2098e9076143db140e1b3832e017ab746
kmeeseek <beaverdi147@gmail.com>
2021-11-26 23:00:37 +0300

    some methods moved outside Command.cpp file

---
+++ parserMsg.cpp
@@ -0,0 +1,36 @@
+#include "Command.hpp"
+
+std::string Command::parseMsg( std::string cmdStr )
+{
+	std::string tmp;
+	std::string::iterator it_begin = cmdStr.begin();
+	std::string::iterator it_end = cmdStr.end();
+	
+	while (*it_begin == ' ')
+	{
+		cmdStr.erase(it_begin);
+		it_begin++;
+	}
+	while (*it_end == ' ')
+	{
+		cmdStr.erase(it_begin);
+		it_end--;
+	}
+	if ( *it_begin == ':')
+	{
+		it_begin++;
+		this->msg.prefx = std::string();
+	}
+	while ( it_begin != it_end)
+	{
+		
+	}
+}
+
+std::string Command::getRidOfCmdName( std::string cmdStr ) {
+    return cmdStr;
+}
+
+std::string checkCmdNameFormat( std::string cmdStr ) {
+    return cmdStr;
+}
---
+++ userBasedQueriesCmd.cpp
@@ -0,0 +1,13 @@
+#include "Command.hpp"
+
+void Command::who( std::string const , User & ) {
+	
+}
+
+void Command::whois( std::string const , User & ) {
+	
+}
+
+void Command::whowas( std::string const , User & ) {
+	
+}
