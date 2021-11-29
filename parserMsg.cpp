20e25681dcdcf33b2e4058796ab3cb7ee055d199
kmeeseek <beaverdi147@gmail.com>
2021-11-29 21:21:03 +0300

    trailing part separated in parser, WHO command results examples added

--- parserMsg.cpp
+++ parserMsg.cpp
@@ -5,7 +5,10 @@ std::string Command::parseMsg(std::string cmdStr)
 {
 	std::string parsed;
 	std::stringstream input(cmdStr);
+	std::vector<std::string>::iterator trailingIt;
+	std::vector<std::string>::iterator cutIt;
 
+	//the loop below split string to tokens separated by space
 	while (std::getline(input, parsed, ' '))
 	{
 		if (!parsed.empty())
@@ -18,6 +21,29 @@ std::string Command::parseMsg(std::string cmdStr)
 				this->msg.midParams.push_back(parsed);
 		}
 	}
+
+	//identifying if trailing is presented amond mid parameters
+	trailingIt = this->msg.midParams.begin();
+	while (trailingIt != this->msg.midParams.end())
+	{
+		if ((*trailingIt)[0] == ':')
+			break;
+		trailingIt++;
+	}
+
+	//saving iterator to the trailing part in vector
+	cutIt = trailingIt;
+
+	//rewriting all mid params from vector to prefix attribute beginning from one with ':'
+	while (trailingIt != this->msg.midParams.end())
+	{
+		this->msg.trailing += " " + *trailingIt;
+		trailingIt++;
+	}
+	this->msg.trailing.erase(this->msg.trailing.begin());
+
+	// cuting trailing part from mid params array
+	this->msg.midParams.erase(cutIt, this->msg.midParams.end());
 
 	// // check if command and at least one param are presented in message, return error
 	// // if (1)
--- userBasedQueriesCmd.cpp
+++ userBasedQueriesCmd.cpp
@@ -2,6 +2,23 @@
 
 void Command::who( std::string const &, User & ) {
 	
+	if 
+	// пример команды и возвращаемого значения:
+
+	// WHO *
+	// :IRCat 352 kin * 1 127.0.0.1 IRCat nik H :0 4
+	// :IRCat 352 kin * 1 127.0.0.1 IRCat kin H :0 4
+	// :IRCat 315 kin kin :End of /WHO list
+
+	// WHO
+	// :IRCat 461 nik WHO :Not enough parameters
+
+	// WHO ***
+	// :IRCat 352 nik * 1 127.0.0.1 IRCat nik H :0 4
+	// :IRCat 315 nik nik :End of /WHO list
+
+	// WHO 1 2 3 4
+	// :IRCat 315 nik nik :End of /WHO list
 }
 
 void Command::whois( std::string const &, User & ) {
