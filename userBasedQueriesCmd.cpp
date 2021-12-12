856921360a09e296b2f2e88f2cbb3277b86c37aa
kmeeseek <beaverdi147@gmail.com>
2021-12-12 22:52:46 +0300

    whois started

--- userBasedQueriesCmd.cpp
+++ userBasedQueriesCmd.cpp
@@ -11,11 +11,12 @@ int Server::who( User & user)
 
 	if (msg.midParams.size() > 2)
 	{
-		std::cout << "invalid number of parameters is given\n";
+		send(user.getFd(), "invalid number of parameters is given\n", 39, 0);
+		// std::cout << "invalid number of parameters is given\n";
 		return (1);
 	}
 
-	if (msg.midParams.size() == 1)
+	if (msg.midParams.size() == 1 || msg.midParams.size() == 0)
 	{
 		while (userIt != endIt)
 		{
@@ -107,22 +108,76 @@ int Server::who( User & user)
 	// WHO * o
 	// :IRCat 315 mynick mynick :End of /WHO list
 
-int Server::whois( User & )
+int Server::whois( User & user)
 {
+	std::vector<User *>::iterator userIt;
+	std::vector<User *>::iterator endUserIt;
+	std::vector<std::string>::iterator paramIt;
+	std::vector<std::string>::iterator endPramIt;
+
+	// if (msg.midParams.size() < 1)
+	// {
+	// 	errorMEss(431, user, 0); 
+	// 	return (1);
+	// }
+	// if (msg.midParams.size() == 1 || msg.midParams.size() == 0)
+	// {
+	// 	while (userIt != endIt)
+	// 	{
+	// 		if (!(*userIt)->empty())
+	// 		{
+	// 			message = "* " + (*userIt)->getUser() + " " + (*userIt)->getHostn() + " " \
+	// 			+ (*userIt)->getServern() + " " + (*userIt)->getNick() + "\n";
+	// 			replyMEss(352, user, message);
+	// 		}
+	// 		userIt++;
+	// 	}
+	// 	std::cout << msg.midParams[0] << " :End of /WHO list\n";
+	// }
 	return 0;	
 }
 
-// ERR_NOSUCHSERVER
-// ERR_NONICKNAMEGIVEN
-// RPL_WHOISUSER
-// RPL_WHOISCHANNELS
-// RPL_WHOISCHANNELS
-// RPL_WHOISSERVER
-// RPL_AWAY
-// RPL_WHOISOPERATOR
-// RPL_WHOISIDLE
-// ERR_NOSUCHNICK
-// RPL_ENDOFWHOIS
+//? ERR_NOSUCHSERVER 402
+	// "<server name> :No such server"
+    // - Used to indicate the server name given currently
+    //   doesn't exist.
+//DONE ERR_NONICKNAMEGIVEN 431
+    // ":No nickname given"
+    // - Returned when a nickname parameter expected for a
+    //   command and isn't found.
+// RPL_WHOISUSER 311
+    // "<nick> <user> <host> * :<real name>"
+// RPL_WHOISCHANNELS 319
+	// "<nick> :{[@|+]<channel><space>}"
+	//   - Replies 311 - 313, 317 - 319 are all replies
+    // generated in response to a WHOIS message.  Given that
+    // there are enough parameters present, the answering
+    // server must either formulate a reply out of the above
+    // numerics (if the query nick is found) or return an
+    // error reply.  The '*' in RPL_WHOISUSER is there as
+    // the literal character and not as a wild card.  For
+    // each reply set, only RPL_WHOISCHANNELS may appear
+    // more than once (for long lists of channel names).
+    // The '@' and '+' characters next to the channel name
+    // indicate whether a client is a channel operator or
+    // has been granted permission to speak on a moderated
+    // channel.  The RPL_ENDOFWHOIS reply is used to mark
+    // the end of processing a WHOIS message.
+// RPL_WHOISSERVER 312
+	// "<nick> <server> :<server info>"
+// RPL_AWAY 301
+	// "<nick> :<away message>"
+// RPL_WHOISOPERATOR 313
+	// "<nick> :is an IRC operator"
+// RPL_WHOISIDLE 317
+	// "<nick> <integer> :seconds idle"
+// ERR_NOSUCHNICK 401
+	// "<nickname> :No such nick/channel"
+    // - Used to indicate the nickname parameter supplied to a
+    //   command is currently unused.
+
+// RPL_ENDOFWHOIS 318
+	// "<nick> :End of /WHOIS list"
 
 int Server::whowas( User & )
 {
