5018050766c21275fa8c0b99f734ee19dad89640
kmeeseek <beaverdi147@gmail.com>
2021-12-15 22:59:15 +0300

    WHO implrmrnted, WWHOIS started

--- userBasedQueriesCmd.cpp
+++ userBasedQueriesCmd.cpp
@@ -10,130 +10,154 @@ int Server::who( User & user)
 	endIt = userData.end();
 
 	if (msg.midParams.size() > 2)
+		return (errorMEss(1000, user, "invalid number of parameters is given"));
+	else if (msg.midParams.size() == 2 && msg.midParams[1] != "o")
+		return (errorMEss(1000, user, \
+		"second parameters of command is invalid, please put \"o\" as a second parameter if you want to see the list of operators only"));
+
+	//rfc1459 4.5.1 "In the absence of the <name> parameter, all visible
+	//(users who aren't invisible (user mode +i) and who don't have a
+	//common channel with the requesting client) are listed.  The same
+	//result can be achieved by using a <name> of "0" or any wildcard which
+	// will end up matching every entry possible."
+	if (msg.midParams[0] == "0" || msg.midParams[0] == "*" || msg.midParams.size() == 0 \
+	|| (msg.midParams.size() == 1 && msg.midParams[0] == "o"))
 	{
-		send(user.getFd(), "invalid number of parameters is given\n", 39, 0);
-		// std::cout << "invalid number of parameters is given\n";
-		return (1);
+		while (userIt != endIt)
+		{
+			if ((*userIt)->getChannelList().empty())
+				message = "* " + (*userIt)->getUser() \
+						+ " " + (*userIt)->getHost() + " " + (*userIt)->getServer() + " " \
+						+ (*userIt)->getNick() + "\n";
+			else
+				message = *((*userIt)->getChannelList().end() - 1) + (*userIt)->getUser() \
+						+ " " + (*userIt)->getHost() + " " + (*userIt)->getServer() + " " \
+						+ (*userIt)->getNick() + "\n";
+			if (((msg.midParams.size() == 2 && msg.midParams[1] == "o") \
+				|| (msg.midParams.size() == 1 && msg.midParams[0] == "o")) \
+				&& !(*userIt)->getOpChannelList().empty())
+			{
+				replyMEss(RPL_WHOREPLY, user, message);
+			}
+			else if(!((msg.midParams.size() == 2 && msg.midParams[1] == "o") \
+					|| (msg.midParams.size() == 1 && msg.midParams[0] == "o")) )
+			{
+				replyMEss(RPL_WHOREPLY, user, message);
+			}
+			userIt++;
+		}
+		//The RPL_WHOREPLY and RPL_ENDOFWHO pair are used
+		//to answer a WHO message.  The RPL_WHOREPLY is only
+		//sent if there is an appropriate match to the WHO
+		//query.  If there is a list of parameters supplied
+		//with a WHO message, a RPL_ENDOFWHO must be sent
+		//after processing each list item with <name> being
+		//the item.
+		replyMEss(RPL_ENDOFWHO, user, "");
+	}
+	else if (msg.midParams.size() == 1)
+	{
+		//The <name> passed to WHO is matched against users' host, server, real
+		//name and nickname if the channel <name> cannot be found.
+		while (userIt != endIt)
+		{
+			if ((*userIt)->getChannelList().empty())
+			{
+				message = "* " + (*userIt)->getUser() \
+						+ " " + (*userIt)->getHost() + " " + (*userIt)->getServer() + " " \
+						+ (*userIt)->getNick() + "\n";
+			}
+			else
+				message = *((*userIt)->getChannelList().end() - 1) + (*userIt)->getUser() \
+						+ " " + (*userIt)->getHost() + " " + (*userIt)->getServer() + " " \
+						+ (*userIt)->getNick() + "\n";
+			if (checkWildcard((*userIt)->getUser().c_str(), msg.midParams[0].c_str()))
+				replyMEss(RPL_WHOREPLY, user, message);
+			else if (checkWildcard((*userIt)->getHost().c_str(), msg.midParams[0].c_str()))
+				replyMEss(RPL_WHOREPLY, user, message);
+			else if (checkWildcard((*userIt)->getServer().c_str(), msg.midParams[0].c_str()))
+				replyMEss(RPL_WHOREPLY, user, message);
+			else if (checkWildcard((*userIt)->getReal().c_str(), msg.midParams[0].c_str()))
+				replyMEss(RPL_WHOREPLY, user, message);
+			else if (checkWildcard((*userIt)->getNick().c_str(), msg.midParams[0].c_str()))
+				replyMEss(RPL_WHOREPLY, user, message);
+			userIt++;
+		}
+		replyMEss(RPL_ENDOFWHO, user, "");
+	}
+	else
+	{
+		std::cout << "if_3\n";
+		while (userIt != endIt)
+		{
+			if (!(*userIt)->getOpChannelList().empty())
+			{
+				if ((*userIt)->getChannelList().empty())
+					message = "* " + (*userIt)->getUser() \
+							+ " " + (*userIt)->getHost() + " " + (*userIt)->getServer() + " " \
+							+ (*userIt)->getNick() + "\n";
+				else
+					message = *((*userIt)->getChannelList().end() - 1) + (*userIt)->getUser() \
+							+ " " + (*userIt)->getHost() + " " + (*userIt)->getServer() + " " \
+							+ (*userIt)->getNick() + "\n";
+				if (checkWildcard((*userIt)->getUser().c_str(), msg.midParams[0].c_str()))
+					replyMEss(RPL_WHOREPLY, user, message);
+				else if (checkWildcard((*userIt)->getHost().c_str(), msg.midParams[0].c_str()))
+					replyMEss(RPL_WHOREPLY, user, message);
+				else if (checkWildcard((*userIt)->getServer().c_str(), msg.midParams[0].c_str()))
+					replyMEss(RPL_WHOREPLY, user, message);
+				else if (checkWildcard((*userIt)->getReal().c_str(), msg.midParams[0].c_str()))
+					replyMEss(RPL_WHOREPLY, user, message);
+				else if (checkWildcard((*userIt)->getNick().c_str(), msg.midParams[0].c_str()))
+					replyMEss(RPL_WHOREPLY, user, message);
+			}
+			userIt++;
+		}
+		replyMEss(RPL_ENDOFWHO, user, "");
+	}
+	return (0);
+}
+
+int Server::whois( User & user)
+{
+	// [<server>] parameter of command is not checked
+	std::string message;
+
+	std::vector<User *>::iterator userIt = userData.begin();
+	std::vector<User *>::iterator endUserIt = userData.end();
+	std::vector<std::string>::iterator paramIt = msg.midParams.begin();
+	std::vector<std::string>::iterator endPramIt = msg.midParams.end();
+
+	while (paramIt != endPramIt)
+	{
+		message = (*userIt)->getNick() + " " + (*userIt)->getUser() \
+				+ " " + (*userIt)->getHost() + " * :" + (*userIt)->getReal() + "\n";
+		while (userIt != endUserIt)
+		{
+			replyMEss(RPL_ENDOFWHO, user, "");
+			checkWildcard((*userIt)->getHost().c_str(), msg.midParams[0].c_str())
+		}
 	}
 
+	if (msg.midParams.size() < 1)
+	{
+		errorMEss(431, user, 0); 
+		return (1);
+	}
 	if (msg.midParams.size() == 1 || msg.midParams.size() == 0)
 	{
 		while (userIt != endIt)
 		{
 			if (!(*userIt)->empty())
 			{
-				message = "* " + (*userIt)->getUser() + " " + (*userIt)->getHost() + " " \
-				+ (*userIt)->getServer() + " " + (*userIt)->getNick() + "\n";
+				message = "* " + (*userIt)->getUser() + " " + (*userIt)->getHostn() + " " \
+				+ (*userIt)->getServern() + " " + (*userIt)->getNick() + "\n";
 				replyMEss(352, user, message);
 			}
 			userIt++;
 		}
 		std::cout << msg.midParams[0] << " :End of /WHO list\n";
 	}
-
-	else if (msg.midParams.size() == 2) //имеет смысл объединить с верхним условием
-	{
-		while (userIt != endIt)
-		{
-			if (!(*userIt)->empty()) //добавить условие на оператора
-			{
-				message = "* " + (*userIt)->getUser() + " " + (*userIt)->getHost() + " " \
-				+ (*userIt)->getServer() + " " + (*userIt)->getNick() + "\n";
-				replyMEss(352, user, message);
-			}
-			userIt++;
-		}
-		if (msg.midParams[2] != "o")
-			return (1);
-		std::cout << msg.midParams[0] << " :End of /WHO list\n";
-	}
-
-	else
-	{
-	}
-	return (0);
-}
-	// пример команды и возвращаемого значения:
-
-	// WHO *
-	// :IRCat 352 kin * 1 127.0.0.1 IRCat nik H :0 4 //The numeric reply must be sent as one message consisting of the sender prefix, the three digit numeric, and the target of the reply. 
-	// :IRCat 352 kin * 1 127.0.0.1 IRCat kin H :0 4
-	// :IRCat 315 kin kin :End of /WHO list
-
-	// WHO
-	// :IRCat 461 nik WHO :Not enough parameters
-
-	// WHO ***
-	// :IRCat 352 nik * 1 127.0.0.1 IRCat nik H :0 4
-	// :IRCat 315 nik nik :End of /WHO list
-
-	// WHO 1 2 3 4
-	// :IRCat 315 nik nik :End of /WHO list
-
-
-	// WHO
-	// :IRCat 461 mynick WHO :Not enough parameters
-
-	// WHO othernick
-	// :IRCat 352 mynick * user2 127.0.0.1 IRCat othernick H :0  real2 name2
-	// :IRCat 315 mynick mynick :End of /WHO list
-
-	// WHO othernick o
-	// :IRCat 315 mynick mynick :End of /WHO list
-
-	// WHO *
-	// :IRCat 352 mynick * username 127.0.0.1 IRCat mynick H :0  real name
-	// :IRCat 352 mynick * user2 127.0.0.1 IRCat othernick H :0  real2 name2
-	// :IRCat 315 mynick mynick :End of /WHO list
-
-	// WHO othernick f
-	// :IRCat 352 mynick * user2 127.0.0.1 IRCat othernick H :0  real2 name2
-	// :IRCat 315 mynick mynick :End of /WHO list
-
-	// WHO othernick bla bla
-	// :IRCat 352 mynick * user2 127.0.0.1 IRCat othernick H :0  real2 name2
-	// :IRCat 315 mynick mynick :End of /WHO list
-
-	// WHO noone
-	// :IRCat 315 mynick mynick :End of /WHO list
-
-	// WHO noone o
-	// :IRCat 315 mynick mynick :End of /WHO list
-
-	// WHO *
-	// :IRCat 352 mynick * username 127.0.0.1 IRCat mynick H :0  real name
-	// :IRCat 352 mynick * user2 127.0.0.1 IRCat othernick H :0  real2 name2
-	// :IRCat 315 mynick mynick :End of /WHO list
-	
-	// WHO * o
-	// :IRCat 315 mynick mynick :End of /WHO list
-
-int Server::whois( User & user)
-{
-	std::vector<User *>::iterator userIt;
-	std::vector<User *>::iterator endUserIt;
-	std::vector<std::string>::iterator paramIt;
-	std::vector<std::string>::iterator endPramIt;
-
-	// if (msg.midParams.size() < 1)
-	// {
-	// 	errorMEss(431, user, 0); 
-	// 	return (1);
-	// }
-	// if (msg.midParams.size() == 1 || msg.midParams.size() == 0)
-	// {
-	// 	while (userIt != endIt)
-	// 	{
-	// 		if (!(*userIt)->empty())
-	// 		{
-	// 			message = "* " + (*userIt)->getUser() + " " + (*userIt)->getHostn() + " " \
-	// 			+ (*userIt)->getServern() + " " + (*userIt)->getNick() + "\n";
-	// 			replyMEss(352, user, message);
-	// 		}
-	// 		userIt++;
-	// 	}
-	// 	std::cout << msg.midParams[0] << " :End of /WHO list\n";
-	// }
 	return 0;	
 }
 
